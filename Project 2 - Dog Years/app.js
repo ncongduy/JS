deac9151be440f66b73d85aee65b3ffdc6d0547c
Nguyen Cong Duy <ncongduy@gmail.com>
2020-07-30 21:47:45 +0300

    project 2: convert human years to dog years

---
+++ Project 2 - Dog Years/app.js
@@ -0,0 +1,30 @@
+//My age
+const myAge = 31;
+
+//Early two years
+let earlyYears = 2;
+
+//Convert early two years to dog years
+earlyYears *= 10.5;
+
+//Subtract early two years
+let laterYears = myAge - 2;
+
+//Convert remain years to dog years
+laterYears *= 4;
+
+//Check result when converting dog years
+console.log(earlyYears);
+console.log(laterYears);
+
+//Total dog years
+const myAgeInDogYears = earlyYears + laterYears;
+
+//Use string method
+const myName = 'NCD'.toLowerCase();
+
+//Print the statement
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} old in dog years.`)
+
+
+
