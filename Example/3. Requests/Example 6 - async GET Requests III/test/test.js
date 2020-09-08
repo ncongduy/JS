e2df5b9e427d7b83479a687734b2e635f6506b37
Nguyen Cong Duy <ncongduy@gmail.com>
2020-09-08 16:55:31 +0300

    add example Async GET Request III

---
+++ Example/3. Requests/Example 6 - async GET Requests III/test/test.js
@@ -0,0 +1,44 @@
+console.log = function () {};
+const assert = require("chai").assert;
+const fs = require("fs");
+const Structured = require("structured");
+
+const code = fs.readFileSync("public/main.js", "utf8");
+
+describe("", function () {
+  it("", function () {
+    let structureOne = function () {
+      const getSuggestions = async () => {
+        try {
+          if (response.ok) {
+            const jsonResponse = _;
+            renderRawResponse(jsonResponse);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      };
+    };
+
+    let structureTwo = function () {
+      const getSuggestions = async () => {
+        try {
+          if (response.ok) {
+            const jsonResponse = _;
+            renderResponse(jsonResponse);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      };
+    };
+
+    let isMatchOne = Structured.match(code, structureOne);
+    let isMatchTwo = Structured.match(code, structureTwo);
+    assert.isNotOk(
+      isMatchOne,
+      "Did you delete `renderRawResponse(jsonResponse)`?"
+    );
+    assert.isOk(isMatchTwo, "Did you call `renderResponse(jsonResponse)`?");
+  });
+});
